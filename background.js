d7c3bd0c3bd43fe77fcdca36067af9419253a66a
thechutrain <chu.chuyalan@gmail.com>
2019-01-14 19:16:50 -0500

    somewhat working

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+// TODO(DEVELOPER): Change the values below using values from the initialization snippet: Firebase Console > Overview > Add Firebase to your web app.
+// Initialize Firebase
 var config = {
   apiKey: "AIzaSyBZbSuKfgpEdnKBwEd0KynG6N3lBZ4mPxs",
   authDomain: "github-issue-extension.firebaseapp.com",
@@ -8,7 +10,27 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// console.log("firebase");
-// chrome.identity.getAuthToken({ iteractive: true }, function(token) {
-//   return token;
-// });
+/**
+ * initApp handles setting up the Firebase context and registering
+ * callbacks for the auth status.
+ *
+ * The core initialization is in firebase.App - this is the glue class
+ * which stores configuration. We provide an app name here to allow
+ * distinguishing multiple app instances.
+ *
+ * This method also registers a listener with firebase.auth().onAuthStateChanged.
+ * This listener is called when the user is signed in or out, and that
+ * is where we update the UI.
+ *
+ * When signed in, we also authenticate to the Firebase Realtime Database.
+ */
+function initApp() {
+  // Listen for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    console.log('User state change detected from the Background script of the Chrome Extension:', user);
+  });
+}
+
+window.onload = function() {
+  initApp();
+};
