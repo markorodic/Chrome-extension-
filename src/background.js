8ffac6c890e0aa2b7dcdad5a73a7edda96ffdfa1
thechutrain <chu.chuyalan@gmail.com>
2019-01-15 11:51:00 -0500

    formatted code

--- src/background.js
+++ src/background.js
@@ -1,43 +1,44 @@
+'use strict';
+
 var config = {
-  apiKey: "AIzaSyBZbSuKfgpEdnKBwEd0KynG6N3lBZ4mPxs",
-  authDomain: "github-issue-extension.firebaseapp.com",
-  databaseURL: "https://github-issue-extension.firebaseio.com",
-  projectId: "github-issue-extension",
-  storageBucket: "github-issue-extension.appspot.com",
-  messagingSenderId: "765664737086"
+  apiKey: 'AIzaSyBZbSuKfgpEdnKBwEd0KynG6N3lBZ4mPxs',
+  authDomain: 'github-issue-extension.firebaseapp.com',
+  databaseURL: 'https://github-issue-extension.firebaseio.com',
+  projectId: 'github-issue-extension',
+  storageBucket: 'github-issue-extension.appspot.com',
+  messagingSenderId: '765664737086',
 };
 firebase.initializeApp(config);
 
 const provider = new firebase.auth.GithubAuthProvider();
-// provider.addScope("repo");
 
 let userData = {
-  userToken: "",
-  user: ""
+  userToken: '',
+  user: '',
 };
 
 firebase
   .auth()
   .signInWithPopup(provider)
   .then(function(result) {
-    console.log('successful log in')
+    console.log('successful log in');
     // This gives you a GitHub Access Token. You can use it to access the GitHub API.
     const token = result.credential.accessToken;
     userData.userToken = token;
     // The signed-in user info.
     const user = result.user;
     userData.user = user;
-    
+
     // makeGithubRequest(token).then(data => {
     //   console.log(data);
     // });
-  
+
     makeIssueRequest(token).then(data => {
-      console.log(data)
-    })
+      console.log(data);
+    });
   })
   .catch(function(error) {
-    console.log('Error', error)
+    console.log('Error', error);
 
     // Handle Errors here.
     const errorCode = error.code;
@@ -50,7 +51,7 @@ firebase
   });
 
 function makeGithubRequest(token) {
-  const baseUrl = `https://api.github.com/graphql?access_token=${token}`
+  const baseUrl = `https://api.github.com/graphql?access_token=${token}`;
 
   let query = {
     query: `{
@@ -62,26 +63,25 @@ function makeGithubRequest(token) {
            }
          }
        }
-    }` 
-  }
+    }`,
+  };
 
-  return new Promise(function(resolve, reject){
+  return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", baseUrl, true);
+    xhr.open('POST', baseUrl, true);
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
+      if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
         var resp = JSON.parse(xhr.responseText);
         resolve(resp);
       }
-    }
+    };
     xhr.send(JSON.stringify(query));
-  })
+  });
 }
 
-
 function makeIssueRequest(token, repo) {
-  const baseUrl = `https://api.github.com/graphql?access_token=${token}`
+  const baseUrl = `https://api.github.com/graphql?access_token=${token}`;
 
   // let mutation = {
   //   mutation: `{
@@ -95,7 +95,7 @@ function makeIssueRequest(token, repo) {
   //       clientMutationId,
   //       issue
   //      }
-  //   }` 
+  //   }`
   // }
 
   let mutation = {
@@ -108,21 +108,20 @@ function makeIssueRequest(token, repo) {
           id
         }
       }
-    }`
-  }
+    }`,
+  };
 
-  return new Promise(function(resolve, reject){
+  return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", baseUrl, true);
+    xhr.open('POST', baseUrl, true);
     // xhr.setRequestHeader("Accept", "application/vnd.github.starfire-preview+json")
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
+      if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
         var resp = JSON.parse(xhr.responseText);
         resolve(resp);
       }
-    }
+    };
     xhr.send(JSON.stringify(mutation));
-  })
+  });
 }
-
