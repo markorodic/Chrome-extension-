217ad31fc7c9a6dcbbc2eae7e1c95bd78c73fb65
thechutrain <chu.chuyalan@gmail.com>
2019-01-15 00:01:36 -0500

    current version of our graphql http request

--- background.js
+++ background.js
@@ -27,11 +27,14 @@ firebase
     // The signed-in user info.
     const user = result.user;
     userData.user = user;
-    // 
-    makeGithubRequest(token).then(data => {
-      console.log(data);
-    });
+    
+    // makeGithubRequest(token).then(data => {
+    //   console.log(data);
+    // });
   
+    makeIssueRequest(token).then(data => {
+      console.log(data)
+    })
   })
   .catch(function(error) {
     console.log('Error', error)
@@ -47,12 +50,24 @@ firebase
   });
 
 function makeGithubRequest(token) {
-  const apiEndPoint = `user/repos`
-  const baseUrl = `https://api.github.com/${apiEndPoint}?access_token=${token}`;
-  
+  const baseUrl = `https://api.github.com/graphql?access_token=${token}`
+
+  let query = {
+    query: `{
+      viewer {
+        name
+         repositories(last: 4) {
+           nodes {
+             name
+           }
+         }
+       }
+    }` 
+  }
+
   return new Promise(function(resolve, reject){
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", baseUrl, true);
+    xhr.open("POST", baseUrl, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
@@ -60,7 +75,54 @@ function makeGithubRequest(token) {
         resolve(resp);
       }
     }
-    xhr.send();
+    xhr.send(JSON.stringify(query));
+  })
+}
+
+
+function makeIssueRequest(token, repo) {
+  const baseUrl = `https://api.github.com/graphql?access_token=${token}`
+
+  // let mutation = {
+  //   mutation: `{
+  //     Mutation.createIssue(input: {
+  //       assigneeIds: "MDQ6VXNlcjczNjQ4MDA=",
+  //       labelIds: "MDU6TGFiZWwzMTgxMjg3NzA=",
+  //       projectIds: "MDc6UHJvamVjdDIwNzU3MDc=",
+  //       repositoryId: "MDEwOlJlcG9zaXRvcnk1MDYwNDk5Nw==",
+  //       title: "YOOO graphQL is boss"
+  //     }) {
+  //       clientMutationId,
+  //       issue
+  //      }
+  //   }` 
+  // }
+
+  let mutation = {
+    mutation: `{
+      addReaction(input:{subjectId:"MDU6SXNzdWUyMzEzOTE1NTE=",content:HOORAY}) {
+        reaction {
+          content
+        }
+        subject {
+          id
+        }
+      }
+    }`
+  }
+
+  return new Promise(function(resolve, reject){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", baseUrl, true);
+    // xhr.setRequestHeader("Accept", "application/vnd.github.starfire-preview+json")
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+        // JSON.parse does not evaluate the attacker's scripts.
+        var resp = JSON.parse(xhr.responseText);
+        resolve(resp);
+      }
+    }
+    xhr.send(JSON.stringify(mutation));
   })
 }
 
