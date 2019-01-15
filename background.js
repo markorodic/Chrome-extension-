37673ba8719890fbda5e48435182af9f9b5da978
markorodic <mrmarkorodic@googlemail.com>
2019-01-14 19:04:14 -0500

    Add firebase

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
-console.log("we are making a chrome extension!");
+var config = {
+  apiKey: "AIzaSyBZbSuKfgpEdnKBwEd0KynG6N3lBZ4mPxs",
+  authDomain: "github-issue-extension.firebaseapp.com",
+  databaseURL: "https://github-issue-extension.firebaseio.com",
+  projectId: "github-issue-extension",
+  storageBucket: "github-issue-extension.appspot.com",
+  messagingSenderId: "765664737086"
+};
+firebase.initializeApp(config);
 
-chrome.identity.getAuthToken({'iteractive': true}, function(token) {
-  
-})
-
-
+// console.log("firebase");
+// chrome.identity.getAuthToken({ iteractive: true }, function(token) {
+//   return token;
+// });
