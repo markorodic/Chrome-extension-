ca22d5161dab3327a3d4cdadaa1ecd697077be5d
markorodic <mrmarkorodic@googlemail.com>
2019-01-14 19:38:28 -0500

    Add firebase github provider auth

--- background.js
+++ background.js
@@ -8,6 +8,36 @@ var config = {
 };
 firebase.initializeApp(config);
 
+const provider = new firebase.auth.GithubAuthProvider();
+// provider.addScope("repo");
+
+let userData = {
+  userToken: "",
+  user: ""
+};
+
+firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then(function(result) {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    const token = result.credential.accessToken;
+    userData.userToken = token;
+    // The signed-in user info.
+    const user = result.user;
+    userData.user = user;
+    // ...
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    const credential = error.credential;
+    // ...
+  });
 // console.log("firebase");
 // chrome.identity.getAuthToken({ iteractive: true }, function(token) {
 //   return token;
