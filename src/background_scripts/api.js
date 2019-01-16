4270e33a06b6aa65078908f9a8cb31851c8572a1
thechutrain <chu.chuyalan@gmail.com>
2019-01-15 23:37:46 -0500

    working mutation query with some fetch

--- src/background_scripts/api.js
+++ src/background_scripts/api.js
@@ -1,5 +1,56 @@
 'use strict';
 
+function makeIssueFetch(token) {
+  const baseUrl = `https://api.github.com/graphql?access_token=${token}`;
+  var mutation = `mutation {
+    createIssue(input: {
+      assigneeIds: "MDQ6VXNlcjczNjQ4MDA=",
+      labelIds: "MDU6TGFiZWwzMTgxMjg3NzA=",
+      projectIds: "MDc6UHJvamVjdDIwNzU3MDc=",
+      repositoryId: "MDEwOlJlcG9zaXRvcnk1MDYwNDk5Nw==",
+      title: "Paul is a graphql herooooooooooooooooooooo!"
+    }) {
+      clientMutationId
+    }
+  }`;
+
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/vnd.github.starfire-preview+json',
+    },
+    body: JSON.stringify({ query: mutation }),
+  }).then(response => {
+    return response.json();
+  });
+}
+
+function queryFetch(token) {
+  const baseUrl = `https://api.github.com/graphql?access_token=${token}`;
+  const query = {
+    query: `{
+      viewer {
+        name
+         repositories(last: 4) {
+           nodes {
+             name
+           }
+         }
+       }
+    }`,
+  };
+
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/vnd.github.starfire-preview+json',
+    },
+    body: JSON.stringify(query),
+  }).then(response => {
+    return response.json();
+  });
+}
+
 // eslint-disable-next-line
 function makeGithubRequest(token) {
   const baseUrl = `https://api.github.com/graphql?access_token=${token}`;
@@ -35,50 +86,76 @@ function makeGithubRequest(token) {
 function makeIssueRequest(token, repo) {
   const baseUrl = `https://api.github.com/graphql?access_token=${token}`;
 
+  // let mutation = {
+  //   mutation: `{
+  //     createIssue(input: {
+  //       assigneeIds: "MDQ6VXNlcjczNjQ4MDA=",
+  //       labelIds: "MDU6TGFiZWwzMTgxMjg3NzA=",
+  //       projectIds: "MDc6UHJvamVjdDIwNzU3MDc=",
+  //       repositoryId: "MDEwOlJlcG9zaXRvcnk1MDYwNDk5Nw==",
+  //       title: "YOOO graphQL is boss"
+  //     }) {
+  //       issue {
+  //         id
+  //       }
+  //      }
+  //   }`,
+  // };
+
   let mutation = {
     mutation: `{
-      createIssue(input: {
-        assigneeIds: "MDQ6VXNlcjczNjQ4MDA=",
-        labelIds: "MDU6TGFiZWwzMTgxMjg3NzA=",
-        projectIds: "MDc6UHJvamVjdDIwNzU3MDc=",
-        repositoryId: "MDEwOlJlcG9zaXRvcnk1MDYwNDk5Nw==",
-        title: "YOOO graphQL is boss"
+      addReaction(input: {
+        subjectId: "MDU6SXNzdWUzOTkxNzg4NjQ=",
+        content:HEART
       }) {
-        issue {
-          id
+        reaction {
+          clientMutationId
         }
-       }
+      }
     }`,
   };
 
   // let mutation = {
   //   mutation: `{
-  //       addReaction(input:{subjectId:"MDU6SXNzdWUyMzEzOTE1NTE=",content:HOORAY}) {
-  //         reaction {
-  //           content
-  //         }
-  //         subject {
-  //           id
-  //         }
+  //     createIssue(input: {
+  //       assigneeIds: "MDQ6VXNlcjczNjQ4MDA=",
+  //       labelIds: "MDU6TGFiZWwzMTgxMjg3NzA=",
+  //       projectIds: "MDc6UHJvamVjdDIwNzU3MDc=",
+  //       repositoryId: "MDEwOlJlcG9zaXRvcnk1MDYwNDk5Nw==",
+  //       title: "YOOO graphQL is boss"
+  //     }) {
+  //       clientMutationId,
+  //       issue {
+  //        id
   //       }
-  //     }`,
+  //     }
+  //   }`,
   // };
 
   return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
+    // xhr.responseType = 'json';
     xhr.open('POST', baseUrl, true);
+    // xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader(
       'Accept',
       'application/vnd.github.starfire-preview+json'
     );
+
+    // xhr.onload = function() {
+    //   console.log(xhr.response);
+    // };
+
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
         var resp = JSON.parse(xhr.responseText);
-        console.log(resp.error);
+        console.log(JSON.stringify(resp));
         resolve(resp);
       }
     };
+
     xhr.send(JSON.stringify(mutation));
+    // xhr.send(mutation);
   });
 }
