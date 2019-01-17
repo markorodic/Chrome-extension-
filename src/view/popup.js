a912b5318bf4fb95753eb40f5e8ed2ef7049e9ba
thechutrain <chu.chuyalan@gmail.com>
2019-01-17 16:37:55 -0500

    refactored view code organization with event listeners

--- src/view/popup.js
+++ src/view/popup.js
@@ -1,24 +1,27 @@
-/* global VIEW_IS_LOGGED_IN, VIEW_PROMPT_AUTH */
-function isUserLoggedIn() {
-  return new Promise((resolve, reject) => {
-    chrome.runtime.sendMessage(
-      { messageType: VIEW_IS_LOGGED_IN },
-      ({ loginState }) => {
-        loginState ? resolve({ loginState }) : reject({ loginState });
-      }
-    );
-  });
-}
+/* global VIEW_IS_LOGGED_IN onDocumentLoaded */
 
-isUserLoggedIn().then(loggedIn => {
-  console.log(loggedIn);
-  if (loggedIn.loginState) {
+isUserLoggedIn().then(({ loggedIn }) => {
+  if (loggedIn) {
     renderIssueScreen();
   } else {
     renderLoginScreen();
   }
 });
 
+onDocumentLoaded();
+// -------- function declarations ------
+function isUserLoggedIn() {
+  return new Promise(resolve => {
+    chrome.runtime.sendMessage(
+      { messageType: VIEW_IS_LOGGED_IN },
+      ({ loginState }) => {
+        loginState ? resolve({ loggedIn: true }) : resolve({ loggedIn: false });
+      }
+    );
+  });
+}
+
+// ======== VIEW FUNCTIONS =============
 function renderLoginScreen() {
   const loginScreenElement = document.getElementById('login-screen');
   const issueScreenElement = document.getElementById('issue-screen');
@@ -47,37 +50,4 @@ function renderSuccessScreen() {
   loginScreenElement.classList.add('hidden');
   issueScreenElement.classList.add('hidden');
   postSuccessScreen.classList.remove('hidden');
-}
-
-function authenticateUser() {
-  console.log('button pressed login');
-  chrome.runtime.sendMessage({ messageType: VIEW_PROMPT_AUTH }, response => {
-    console.log(response);
-  });
-}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(request);
-});
-
-onDocumentLoaded();
-
-function onDocumentLoaded() {
-  if (
-    document.attachEvent
-      ? document.readyState === 'complete'
-      : document.readyState !== 'loading'
-  ) {
-    console.log('dom was loaded');
-    registerPopupListeners();
-  } else {
-    console.log('DOM content has not yet been loaded');
-    document.addEventListener('DOMContentLoaded', registerPopupListeners);
-  }
-}
-
-function registerPopupListeners() {
-  console.log('firing register listeners');
-  const loginButtonElement = document.getElementById('login-github-btn');
-  loginButtonElement.addEventListener('click', authenticateUser);
 }
---
+++ src/view/viewEventListeners.js
@@ -0,0 +1,68 @@
+/* global  VIEW_PROMPT_AUTH FORM_SUBMISSION  */
+
+// eslint-disable-next-line
+function onDocumentLoaded() {
+  registerChromeMessageListeners();
+
+  if (
+    document.attachEvent
+      ? document.readyState === 'complete'
+      : document.readyState !== 'loading'
+  ) {
+    console.log('dom was loaded');
+    registerViewEventListeners();
+  } else {
+    console.log('DOM content has not yet been loaded');
+    document.addEventListener('DOMContentLoaded', registerViewEventListeners);
+  }
+}
+
+function registerViewEventListeners() {
+  console.log('registered events!!!');
+  const loginButtonElement = document.getElementById('login-github-btn');
+  loginButtonElement.addEventListener('click', authenticateUser);
+
+  const addIssueBtn = document.getElementById('add-issue');
+  addIssueBtn.addEventListener('submit', submitIssue);
+}
+
+function registerChromeMessageListeners() {
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(`RECEIVED REQUEST: ${request}`);
+  });
+}
+
+function authenticateUser() {
+  console.log('button pressed login');
+  chrome.runtime.sendMessage({ messageType: VIEW_PROMPT_AUTH }, response => {
+    console.log(response);
+  });
+}
+
+function submitIssue(event) {
+  event.preventDefault();
+
+  const formData = {};
+
+  const repository = document.getElementById('repository-selection');
+  const repositoryValue = repository.options[repository.selectedIndex].value;
+  var errorCheckboxValue = document.getElementsByClassName('hide')[0].checked;
+  const consoleError = document.getElementsByClassName('console-error')[0]
+    .innerHTML;
+  var titleTextValue = document.getElementById('issue-form-title').value;
+  var bodyTextValue = document.getElementById('issue-form-body').value;
+
+  formData.repository = repositoryValue;
+  formData.error = errorCheckboxValue ? consoleError : '';
+  formData.title = titleTextValue;
+  formData.issue = bodyTextValue;
+
+  chrome.runtime.sendMessage(
+    { messageType: FORM_SUBMISSION, formData },
+    response => {
+      console.log(response);
+    }
+  );
+
+  return false;
+}
