46e1999b563d5227ce419ba82a4f587e55b413ba
thechutrain <chu.chuyalan@gmail.com>
2019-01-16 22:33:00 -0500

    export all message types from a default object

--- src/messageTypeConstants.js
+++ src/messageTypeConstants.js
@@ -1,12 +1,19 @@
 /**
  * Constants are namespaced and follow naming convention of "[origin]/[event-name]"
  */
+export default {
+  VIEW_IS_LOGGED_IN,
+  VIEW_AUTHENTICATE_USER,
+  BG_LOGIN_PENDING,
+  BG_LOGIN_SUCCESS,
+  BG_LOGIN_FAILURE,
+};
 
 // VIEW origin
-export const VIEW_IS_LOGGED_IN = 'view/is_logged_in';
-export const VIEW_AUTHENTICATE_USER = 'view/authenticate_user';
+const VIEW_IS_LOGGED_IN = 'view/is_logged_in';
+const VIEW_AUTHENTICATE_USER = 'view/authenticate_user';
 
 // BACKGROUND origin
-export const BG_LOGIN_PENDING = 'background/login_pending';
-export const BG_LOGIN_SUCCESS = 'background/login_success';
-export const BG_LOGIN_FAILURE = 'background/login_failure';
+const BG_LOGIN_PENDING = 'background/login_pending';
+const BG_LOGIN_SUCCESS = 'background/login_success';
+const BG_LOGIN_FAILURE = 'background/login_failure';
